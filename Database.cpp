98a3eca50d1d51f50f9dd66ee7678cd7a38ac27b
Benjamin Garza <minibeniitec@gmail.com>
2019-11-14 23:02:43 -0800

    auto

--- Database.cpp
+++ Database.cpp
@@ -1,4 +1,4 @@
-/*  Source: 	Database.cpp
+/*  	Source: 	Database.cpp
  *	Author:		Benjamin A Garza III
  *	Date:		Nov 12, 2019
  *	Class:		CMPS 4910 - Senior Seminar I
@@ -13,14 +13,17 @@ using namespace std;
 
 void printMenu() {
     cout << "\n\nMenu:\n";
-    cout << "0. Help\n";
-    cout << "1. Create user and tablespaces\n";
-    cout << "2. Create all tables\n";
-    cout << "3. Insert default tuples\n";
-    cout << "4. Drop all tables\n";
-    cout << "5. Drope user and tablesapces\n";
-    cout << "6. Query All into file\n";
-    cout << "7. Quit\n";
+    cout << " 1. Create user and tablespaces\n";
+    cout << " 2. Create all tables\n";
+    cout << " 3. Insert default tuples\n";
+    cout << " 4. Drop all tables\n";
+    cout << " 5. Drope user and tablesapces\n";
+    cout << " 6. Query All into file\n";
+    cout << "Easy Menu:\n";
+    cout << " 7. Run Setup\n";
+    cout << " 8. Insert Default Data\n";
+    cout << " 9. Drop entire database\n";
+    cout << "10. Quit\n";
     cout << "Choice: ";
     return;
 }
@@ -42,9 +45,6 @@ int main() {
     int choice = 0;
     do {
         switch (choice) {
-            case 0:
-		cout << "Note: For help enter 0;
-                break;
             case 1: 
                 cout << "Creating User and Tablespaces:\n";
                 system("psql -f Create_User_Tablespaces.psql");
@@ -54,8 +54,9 @@ int main() {
                 system("psql -f Create_Tables.psql restaurant  groupzero");
                 break;
             case 3:
+	    case 8:
                 cout << "Inserting defualt tuples into all tables:\n";
-                system("psql -f Insert_Tuples.psql restaurant groupzero");
+                system("psql -f Insert_All.psql restaurant groupzero");
                 break;
             case 4:
                 cout << "Dropping all tables:\n";
@@ -70,8 +71,14 @@ int main() {
                 system("psql -f Query_All.psql restaurant groupzero");
                 break;
 	    case 7:
-		cout << "Goodbye!" << endl;
-		return 0;
+                cout << "Setting Up Database:\n";
+                system("psql -f Create_User_Tablespaces.psql");
+                system("psql -f Create_Tables.psql restaurant  groupzero");
+                break;
+            case 9:
+		cout << "Destroying database...\n";
+                system("psql -f Drop_Tables.psql restaurant groupzero");
+                system("psql -f Drop_User_Tablespaces.psql restaurant groupzero");
             default:
                 cout << "Error: Please enter a valid option.";
         }
--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
-all: push
+all: compile run clean push
+
+compile: Database.cpp
+	g++ -Wall Database.cpp -o Database.out
+
+run: Database.out
+	./Database.out
+
+clean:
+	rm -rf *.out
+
 
 push: .git
 	git add .
